d95d679ad77522cd37ae6b69c1c87b9b33714b87
GeorgeMcK <george@hookstream.com>
2020-07-06 01:59:49 -0700

    Update example

--- timer.php
+++ timer.php
@@ -3,11 +3,11 @@
 require __DIR__ . '/vendor/autoload.php';
 
 // $nodeJsPath = "~/www/nodejs";
-// $filePath = "~/www/7950ohana.com/public_html/dev/";
+// $filePath = "~/www/example.com/public_html/dev/";
 $output = -1;
 $error = -1;
 
-exec("export PATH=$PATH:~/www/nodejs/bin && node ~/www/7950ohana.com/public_html/dev/index.js " . $_REQUEST['text'] . " one two=three four 2>&1", $output, $error);
+exec("export PATH=$PATH:~/www/nodejs/bin && node ~/www/example.com/public_html/dev/index.js " . $_REQUEST['text'] . " one two=three four 2>&1", $output, $error);
 
 // echo "<pre>$output[0]</pre>";
 // echo print_r($output);
